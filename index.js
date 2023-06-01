ab016418951e2680ee026a32a8b0eb39a44bc16a
Domenique <domogenesis77@gmail.com>
2023-06-01 17:45:13 -0500

    whyyyyyyyyyyyyyyyyyy

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: "11 Broadway"
+};
+
+function updateEmployeeWithKeyAndValue() {
+    const employee = { name: 'Sam',
+    streetAddress: "11 Broadway"};
+    return employee};
+    
+
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value;
+      
+        return employee};
+
+    function deleteFromEmployeeByKey(newEmployee,key, value){
+    newEmployee= {employee};
+    delete employee.key,value
+    return newEmployee
+    };
+
+    function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+        employee[key] = value;
+      
+        return employee};
